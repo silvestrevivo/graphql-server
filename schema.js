086673180fe01b19efa44482bc5c54e4796aa3e0
silvestrevivo <silvestrevivomillan@gmail.com>
2018-12-01 20:53:54 +0100

    first query with dummy data

--- schema.js
+++ schema.js
@@ -7,7 +7,44 @@ const {
   GraphQLNonNull
 } = require('graphql')
 
+// HardCode data
+const customers = [
+  { id: '1', name: 'John Doe', email: 'jdoe@gmail', age: 35 },
+  { id: '2', name: 'Silvestre', email: 'silvestre@gmail', age: 42 },
+  { id: '3', name: 'Evelien', email: 'evelien@gmail', age: 31 },
+]
+
+// Customer Type
+const CustomerType = new GraphQLObjectType({
+  name: 'Customer',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  })
+})
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: () => ({
+    customer: {
+      type: CustomerType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        for (let i = 0; i < customers.length; i++) {
+          if (customers[i].id === args.id) {
+            return customers[i]
+          }
+        }
+      }
+    }
+  })
+})
 
 module.exports = new GraphQLSchema({
-  //...
+  query: RootQuery
 })
